5f2cc2fb5f8f974e415b5c6d5a4717388e595125
Your Name <you@example.com>
2023-10-12 19:24:40 +0530

    Auto Push

--- main.tf
+++ main.tf
@@ -14,9 +14,7 @@ resource "aws_s3_bucket" "name" {
   bucket = var.bucket-name
 }
 
-resource "aws_iam_role" "example" {
+resource "aws_iam_user" "example" {
   name               = "${var.username}-user"
-  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
 
-  
 }
