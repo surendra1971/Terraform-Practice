6b1024044e4999ea77437885f3a0e20699351620
Your Name <you@example.com>
2023-10-12 12:23:37 +0530

    Auto Push

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c1d144c8fdd8d690"
+  ami           = var.os
   instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+variable "os" {
+  type = string
+  default = "ami-0c1d144c8fdd8d690"
+  description = "This is my AMI-ID"
 }
