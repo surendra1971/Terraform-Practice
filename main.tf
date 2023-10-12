af77d2145626823cb9cde42eaa59f5873f7544f8
Your Name <you@example.com>
2023-10-12 11:18:20 +0530

    Auto Push

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = ami-0c1d144c8fdd8d690
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
