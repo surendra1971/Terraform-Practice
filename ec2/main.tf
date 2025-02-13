1e7cd97af082d419458b351b79e2d9dd5aeec09e
Surendra <87142076+surendra1971@users.noreply.github.com>
2025-02-13 19:15:23 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 resource "aws_instance" "example" {
-    ami                        = "ami-0007644809ee80683"
+    ami                        = "ami-0005ee01bca55ab66"
     instance_type              = "t2.micro"
    
 }
