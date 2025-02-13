f6cf4d73362883f9b937f71060ec22fbc3d8d9d5
Surendra <87142076+surendra1971@users.noreply.github.com>
2025-02-13 19:06:27 +0530

    added

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,5 +4,5 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami                        = "ami-0007644809ee80683"
     instance_type              = "t2.micro"
-    security_group_ids         = ["sg-0362c76b9e8a0118a"]
+   
 }
