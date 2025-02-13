22e91cac266c5498b8c870486f67d15930183f75
Surendra <87142076+surendra1971@users.noreply.github.com>
2025-02-13 19:01:05 +0530

    added ec2

---
+++ ec2/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = us-west-2
+}
+resource "aws_instance" "example" {
+    ami                        = ami-0007644809ee80683
+    instance_type              = t2.micro
+    security_group_ids         = sg-0362c76b9e8a0118a
+}
