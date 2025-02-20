f83d7d1759def029152e669485d3a89eedf49970
Your Name <you@example.com>
2025-02-20 17:42:47 +0530

    added

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,4 +1,8 @@
 resource "aws_instance" "example" {
     ami                        =  var.ami_id
     instance_type              =  var.instance_type
+
+    tags = {
+       Name = "Linus_Server_With_Remote_Backend"
+  
 }
