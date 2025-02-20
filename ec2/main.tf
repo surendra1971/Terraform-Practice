5306f4fc53cec6e78953ea94ba580ac2da1c63ed
Your Name <you@example.com>
2025-02-20 18:37:10 +0530

    Your commit message

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
     ami                        =  var.ami_id
     instance_type              =  var.instance_type
-    security_groups =             [aws_security_group.allows_all]
+    security_groups =             [aws_security_group.allows_all.id]
     tags = {
        Name = "Linus_Server_With_Remote_Backend"
   
