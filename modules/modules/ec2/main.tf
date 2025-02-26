bdacb5e76557136d3c8cffb867d751e668eb7014
Your Name <you@example.com>
2025-02-26 15:34:45 +0530

    Your commit message

--- modules/modules/ec2/main.tf
+++ modules/modules/ec2/main.tf
@@ -2,7 +2,32 @@ resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
- provisioner "remote-exec" {
+}
+
+resource "aws_security_group" "allows_all" {
+  name        = "allows_tls"
+  description = "Allows SSH inbound traffic"
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allows_tls"
+  }
+
+  provisioner "remote-exec" {
     inline = [
       "echo 'Hello from Terraform' > /home/ubuntu/hello.txt",
       "sudo yum update",
@@ -15,5 +40,8 @@ resource "aws_instance" "example" {
       private_key = file("/home/ec2-user/Terraform-Practice/terraform.pem")
       host        = self.public_ip
     }
-  }
+ }
 }
+
+
+
