c78440157f2a5619ee924fe357de4ce8756a58dd
Your Name <you@example.com>
2025-02-26 15:37:42 +0530

    Your commit message

--- modules/modules/ec2/main.tf
+++ modules/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
+  security_groups =             [aws_security_group.allows_all.name]
 }
 
 resource "aws_security_group" "allows_all" {
@@ -38,7 +38,7 @@ resource "aws_security_group" "allows_all" {
       type        = "ssh"
       user        = "ec2-user"
       private_key = file("/home/ec2-user/Terraform-Practice/terraform.pem")
-      host        = self.public_ip
+      host        = aws_instance.example.public_ip
     }
  }
 }
