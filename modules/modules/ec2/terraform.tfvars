82fd808f15925a314834fd184847a0bed6641456
Your Name <you@example.com>
2025-02-26 12:49:02 +0530

    Your commit message

---
+++ modules/modules/ec2/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id = "ami-0005ee01bca55ab66"
+instance_type = "t2.medium"
--- modules/modules/ec2/variables.tf
+++ modules/modules/ec2/variables.tf
@@ -1,9 +1,2 @@
-variable "ami_id" {
-  description = "The AMI ID to use for the instance"
-  type        = "ami-0005ee01bca55ab66"
-}
-
-variable "instance_type" {
-  description = "The type of instance to create"
-  type        = "t2.medium"
-}
+variable "ami_id" {}
+variable "instance_type" {}
