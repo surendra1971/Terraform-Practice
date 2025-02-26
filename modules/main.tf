13affacae17e122f0af71cc0c15786017349af6f
Your Name <you@example.com>
2025-02-26 13:01:05 +0530

    Your commit message

--- modules/main.tf
+++ modules/main.tf
@@ -6,7 +6,9 @@ module "ec2_instance" {
   source        = "./modules/ec2"
   ami_id        = "ami-0005ee01bca55ab66"   # Example AMI ID
   instance_type = "t2.micro"
+ 
 }
+
 
 output "ec2_instance_id" {
   value = module.ec2_instance.instance_id
--- modules/modules/ec2/main.tf
+++ modules/modules/ec2/main.tf
@@ -1,4 +1,19 @@
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
+
+ provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from Terraform' > /home/ubuntu/hello.txt",
+      "sudo yum update",
+      "sudo yum install -y nginx"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/mykey.pem")
+      host        = self.public_ip
+    }
+  }
 }
