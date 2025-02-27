0920601cfab65cad4e227e7168645212e126d48e
Your Name <you@example.com>
2025-02-27 18:07:20 +0530

    add

---
+++ terraform-user-data/main.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+  region                    = "us-west-2"
+  shared_credentials_files   = ["~/.aws/credentials"]
+  profile                   = "default"  # or any profile you use
+}
+
+
+
+resource "aws_instance" "example" {
+  ami = "ami-0b9dfa0ef0653492f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.main.id]
+  key_name= "aws_key"
+  //user_data     =  "${file("install_apache.sh")}"
+  user_data     = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World!" > hello.txt
+                  EOF
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress                = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 443
+    }
+  ]
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfMKjQqoCx+F3zqQTvzejXmqvZWyB9bKePJbIEuS8lB surendra@LAPTOP-OGCF8IH7"
+}
+
