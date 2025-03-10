2caf68e3c36031f96c5f4468fb9249ec1602566f
Your Name <you@example.com>
2025-03-10 15:43:53 +0530

    add

--- terraform-project/backend.tf
+++ terraform-project/backend.tf
@@ -1,7 +1,19 @@
 # Root Module
+# terraform {
+#   backend "gcs" {
+#     bucket  = "my-terraform-state-bucket"
+#     prefix  = "terraform/state"
+#   }
+# }
+
 terraform {
-  backend "gcs" {
-    bucket  = "my-terraform-state-bucket"
-    prefix  = "terraform/state"
+  backend "s3" {
+    bucket = "my-aws-terraform-state"
+    key    = "terraform/state"
+    region = "us-east-1"
   }
+}
+
+provider "aws" {
+  region = var.region
 }
--- terraform-project/provider.tf
+++ terraform-project/provider.tf
@@ -1,16 +1,5 @@
-# provider "google" {
-#   project = var.project_id
-#   region  = var.region
-# }
-
-terraform {
-  backend "s3" {
-    bucket = "my-aws-terraform-state"
-    key    = "terraform/state"
-    region = "us-east-1"
-  }
-}
-
 provider "aws" {
-  region = var.region
+#   project = var.project_id
+  region  = var.region
 }
+
