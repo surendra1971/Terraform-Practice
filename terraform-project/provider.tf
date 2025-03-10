fee7dc26daee6f42c79cf62aff7300bf2ac1c68d
Your Name <you@example.com>
2025-03-10 15:41:48 +0530

    add

--- terraform-project/provider.tf
+++ terraform-project/provider.tf
@@ -1,4 +1,16 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
+# provider "google" {
+#   project = var.project_id
+#   region  = var.region
+# }
+
+terraform {
+  backend "s3" {
+    bucket = "my-aws-terraform-state"
+    key    = "terraform/state"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = var.region
 }
